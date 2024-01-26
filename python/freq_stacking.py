9b5908e914f1be9cfd8b421c554e0fbf1c90c282
Hannah Fogel <fogelh@myumanitoba.ca>
2024-01-26 16:48:21 -0600

    add python scripts for data processing and calibration

---
+++ python/freq_stacking.py
@@ -0,0 +1,259 @@
+# Frequency stacking algorithm for processing stepped chirp cw radar data
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import windows, hilbert
+from waveforms import dc_chirp, chirp
+from utilities import trim_time_domain_data, DataLoader
+from typing import List
+from enum import Enum, auto
+
+
+class WindowType(Enum):
+    RECT = auto()
+    HAMMING = auto()
+    TUKEY = auto()
+
+
+class FrequencyStacking:
+    """
+    Frequency-stacking algorithm:
+    1. Take FFT of each baseband sub-pulse: zn[tm] => Zn[fk]
+    2. Apply matched filter to each sub-pulse in frequency domain: Dn[fk] = Zn[fk] * conj(Vn[fk]) where Vn is a phase-coherent reference baseband chirp
+    3. Filter each sub-pulse with window function: Dn[fk] * rect[fk / Bs], where Bs > chirp_bandwidth
+    4. Zero-pad each sub-pulse at both ends s.t. new_num_samples > (num_subpulses * sampling_rate * chirp_duration)
+    5. Shift each sub-pulse to the proper center frequency (circshift)
+    6. Sum the freq-domain sub-pulses
+    7. IFFT
+    """
+
+    def __init__(
+        self, recv_data_list: List, acquisition_params: dict, verbose: bool = True
+    ) -> None:
+        self.verbose = verbose
+        self.recv_data_list = recv_data_list
+        self.num_freqs, self.num_channels, self.num_samples = recv_data_list.shape
+        self.center_freqs = acquisition_params["center_freqs"]
+        self.sampling_rate = acquisition_params["sampling_rate"]
+        self.chirp_bw = acquisition_params["chirp_bw"]
+        self.chirp_duration = acquisition_params["chirp_duration"]
+
+        # Precompute filters and fft freqs
+        self.fft_freqs = np.fft.fftfreq(self.num_samples, d=(1 / self.sampling_rate))
+        self.fft_freqs_shifted = np.fft.fftshift(self.fft_freqs)
+        self.rect_window = np.where(
+            np.abs(self.fft_freqs_shifted) <= (self.chirp_bw * 1.02) / 2, 1, 0
+        )
+        self.ideal_chirp_fd = (
+            np.fft.fft(
+                dc_chirp(
+                    1,
+                    self.chirp_bw,
+                    self.sampling_rate,
+                    self.chirp_duration,
+                    pad_length=0,
+                ),
+                n=self.num_samples,
+            )
+            / self.num_samples
+        )
+
+        self.len_zero_padding = int(
+            (max(self.center_freqs) + self.sampling_rate)
+            / (self.fft_freqs[1] - self.fft_freqs[0])
+        )
+        new_max_freq = np.max(self.fft_freqs) + self.len_zero_padding * (
+            self.fft_freqs[1] - self.fft_freqs[0]
+        )
+        self.padded_freqs = np.linspace(
+            -1 * new_max_freq,
+            new_max_freq,
+            num=self.num_samples + 2 * self.len_zero_padding,
+            endpoint=False,
+        )
+        if self.verbose:
+            print(f"Max center freq in data: {np.max(self.center_freqs) / 1e6} MHz")
+            print(f"Max supported freq after padding: {new_max_freq / 1e6} MHz")
+
+    def compute_sww(self, data_chan: int = 0, reference_chan: int = 1) -> np.ndarray:
+        summed_subpulses_fd = np.zeros(
+            [self.num_samples + 2 * self.len_zero_padding], dtype=np.complex64
+        )
+
+        # Loop over frequencies
+        for ii in range(self.num_freqs):
+            if self.verbose:
+                print(f"Freq {ii+1}/{self.num_freqs}")
+
+            # 1. Compute compressed subpulse spectrum
+            compressed_pulse = self.matched_filter(
+                ii, data_chan, reference_chan, meas_phs_only=True
+            )
+
+            # 2. Filter baseband pulse with rect window having Bs >= Bi
+            fd_signal_filt = np.fft.fftshift(compressed_pulse) * self.rect_window
+
+            # 3. Zero-pad symmetrically about DC
+            fd_signal_padded = np.pad(
+                fd_signal_filt,
+                self.len_zero_padding,
+                "constant",
+                constant_values=(0),
+            )
+
+            # 4. Frequency-shift to correct center freq
+            shift = np.argmin(
+                np.abs(np.fft.ifftshift(self.padded_freqs) - self.center_freqs[ii])
+            )
+            fd_signal_padded_shifted = np.roll(fd_signal_padded, shift)
+
+            # 5. Sum sub-pulses
+            summed_subpulses_fd = summed_subpulses_fd + fd_signal_padded_shifted
+
+        return summed_subpulses_fd
+
+    def gls_filter(
+        self, window_type: WindowType = WindowType.RECT, synthetic: bool = True
+    ):
+        if synthetic:
+            chan_idx_synth = np.inf
+            W = self.compute_sww(chan_idx_synth, chan_idx_synth)
+        else:
+            # TODO: build reference SWW from calibration rx channel
+            raise NotImplementedError
+        min_freq = self.center_freqs[0] - self.chirp_bw / 2
+        max_freq = self.center_freqs[-1] + self.chirp_bw / 2
+        fs_wb = len(W) * (self.padded_freqs[1] - self.padded_freqs[0])
+        M = np.fft.fftshift(
+            np.fft.fft(
+                chirp(
+                    fs_wb,
+                    self.chirp_duration,
+                    min_freq,
+                    max_freq,
+                ),
+                n=len(W),
+            )
+        ) / len(W)
+        if window_type == WindowType.RECT:
+            window = (
+                np.where(np.abs(self.padded_freqs) <= max_freq, 1, 0)
+                + np.where(np.abs(self.padded_freqs) >= min_freq, 1, 0)
+                - 1
+            )
+        elif window_type == WindowType.HAMMING:
+            window = windows.hamming(len(W))
+        else:
+            raise NotImplementedError
+        nonzeros = np.nonzero(W)
+        H = np.zeros_like(W)
+        H[nonzeros] = M[nonzeros] / W[nonzeros] * window[nonzeros]
+        return H / np.max(np.abs(H))
+
+    def matched_filter(
+        self,
+        freq_idx: int,
+        data_chan: int,
+        ref_chan: int,
+        meas_phs_only: bool = False,
+    ) -> np.ndarray:
+        # if channel indices are in range of real connected channels, use experimental data; otherwise use ideal chirp
+        Zi = (
+            np.fft.fft(self.recv_data_list[freq_idx][data_chan]) / self.num_samples
+            if data_chan < self.num_channels
+            else self.ideal_chirp_fd
+        )
+        Vi = (
+            np.fft.fft(self.recv_data_list[freq_idx][ref_chan]) / self.num_samples
+            if ref_chan < self.num_channels
+            else self.ideal_chirp_fd
+        )
+        Di = Zi * np.conj(Vi)
+        if meas_phs_only:
+            return np.abs(Zi * self.ideal_chirp_fd) * np.exp(1j * np.angle(Di))
+        else:
+            return Di
+
+
+def main():
+    input_dir = "/Users/hannah/Documents/TerraWave/test data/"
+    input_filenames = [
+        "sfcw_jan23_12mhzchirp_adjGain.mat",
+        "sfcw_jan23_15mhzchirp_adjGain.mat",
+        # "sfcw_jan22_12mhzchirp_A.mat",
+        # "sfcw_jan22_12mhzchirp_B.mat",
+        # "sfcw_jan22_15mhzchirp_A.mat",
+        # "sfcw_jan22_15mhzchirp_B.mat",
+    ]
+    data_ch_idx = 1
+    ref_ch_idx = 0
+    time_domain_analysis = True
+    gls_flag = True
+
+    for ii in range(len(input_filenames)):
+        Loader = DataLoader()
+        input_filename = input_filenames[ii]
+        Loader.load_mat(input_dir + input_filename)
+        trimmed_data = trim_time_domain_data(
+            Loader.recv_data_list,
+            Loader.p["chirp_duration"] * Loader.p["sampling_rate"],
+        )
+        FreqStacking = FrequencyStacking(trimmed_data, Loader.p, verbose=True)
+        sww_spectrum = FreqStacking.compute_sww(data_ch_idx, ref_ch_idx)
+        if gls_flag:
+            gls_filter = FreqStacking.gls_filter()
+            sww_spectrum = gls_filter * sww_spectrum
+
+        plt.figure(1)
+        plt.subplot(2, 2, ii + 1)
+        plt.title(input_filename)
+        idx_to_plot = np.nonzero(sww_spectrum)
+        plt.plot(
+            FreqStacking.padded_freqs[idx_to_plot] / 1e6,
+            20
+            * np.log10(
+                np.abs(sww_spectrum[idx_to_plot])
+                / np.max(np.abs(sww_spectrum[idx_to_plot]))
+            ),
+        )
+        plt.grid()
+
+        if time_domain_analysis:
+            sww_td = np.fft.fftshift(
+                len(sww_spectrum) * np.real(np.fft.ifft(np.fft.ifftshift(sww_spectrum)))
+            )
+            plt.figure(2)
+            # plt.subplot(2, 2, ii + 1)
+            # plt.title(input_filename)
+            wave_speed = 2e8  # [m/s] -- approx val for coax
+            padded_dt = 1 / (2 * np.max(FreqStacking.padded_freqs))
+            d = wave_speed * np.arange(
+                -len(sww_td) / 2 * padded_dt, len(sww_td) / 2 * padded_dt, padded_dt
+            )
+            idx_to_plot = np.argwhere(abs(d) < 10)
+            sww_td_env = abs(hilbert(sww_td))
+            plt.plot(
+                d[idx_to_plot],
+                20 * np.log10(sww_td_env[idx_to_plot] / max(sww_td_env[idx_to_plot])),
+            )
+            plt.grid()
+
+    plt.figure(1)
+    plt.suptitle(f"CH1 SWW Spectrum")
+    plt.ylim([-80, 0])
+    plt.xlabel("Freq [MHz]")
+    plt.ylabel("Normalized Magnitude [dB]")
+    # plt.grid()
+
+    if time_domain_analysis:
+        plt.figure(2)
+        plt.title("Reconstructed A-scans")
+        plt.grid()
+        plt.xlabel("Range [m]")
+        plt.xlim([-5, 5])
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/utilities.py
@@ -0,0 +1,184 @@
+import logging
+from datetime import datetime
+from typing import List
+import numpy as np
+
+
+def validate_args(min_freq: int, max_freq: int, chirp_bw: int, sampling_rate: int):
+    """
+    Validates user input arguments for the following:
+
+    - Sampling frequency > 2x chirp bandwidth
+    - List of center frequencies in range
+    - Tx and Rx settings in range:
+    """
+
+    # Limits based on USRP documentation
+    MIN_CENTER_FREQ = 70e6
+    MAX_CENTER_FREQ = 6e9
+    MIN_SAMPLING_RATE = 200e3
+    MAX_SAMPLING_RATE = 56e6
+    NYQUIST_SAFETY_FACTOR = 0.2  # recommended not to operate within outer 20% of Nyquist zone (host/docs/general.dox)
+
+    valid: bool = True
+    msg: List[str] = []
+
+    if sampling_rate < MIN_SAMPLING_RATE or sampling_rate > MAX_SAMPLING_RATE:
+        valid = False
+        msg.append(
+            f"Sampling rate {sampling_rate / 1e6} MSps out of range {MIN_SAMPLING_RATE / 1e6}-{MAX_SAMPLING_RATE / 1e6} MSps for device."
+        )
+
+    if min_freq < MIN_CENTER_FREQ:
+        valid = False
+        msg.append(f"Minimum center frequency {min_freq} MHz out of range for device.")
+    if max_freq > MAX_CENTER_FREQ:
+        valid = False
+        msg.append(f"Maximum center frequency {max_freq} MHz out of range for device.")
+
+    if sampling_rate < (2 + NYQUIST_SAFETY_FACTOR) * chirp_bw:
+        valid = False
+        msg.append(
+            f"Sampling rate {sampling_rate / 1e6} MSps less than Nyquist rate + {NYQUIST_SAFETY_FACTOR*100}% for signal bandwidth {chirp_bw / 1e6} MHz."
+        )
+    return valid, msg
+
+
+class LogFormatter(logging.Formatter):
+    """Log formatter which prints the timestamp with fractional seconds"""
+
+    @staticmethod
+    def pp_now():
+        """Returns a formatted string containing the time of day"""
+        now = datetime.now()
+        return "{:%H:%M}:{:05.2f}".format(now, now.second + now.microsecond / 1e6)
+        # return "{:%H:%M:%S}".format(now)
+
+    def formatTime(self, record, datefmt=None):
+        converter = self.converter(record.created)
+        if datefmt:
+            formatted_date = converter.strftime(datefmt)
+        else:
+            formatted_date = LogFormatter.pp_now()
+        return formatted_date
+
+
+def nextpow2(i):
+    """
+    Find 2^n that is equal to or greater than i.
+    """
+    n = 1
+    while n < i:
+        n *= 2
+    return n
+
+
+def trim_time_domain_data(recv_data_list, pulse_duration_samples, plot=False):
+    threshold = 0.3
+    new_num_samples = int(
+        1.0 * pulse_duration_samples
+    )  # this might need to change in future
+    head = 2000
+    num_freqs, num_channels, num_samples = recv_data_list.shape
+    data_trimmed = np.empty(
+        [num_freqs, num_channels, new_num_samples], dtype=np.complex64
+    )
+    if plot:
+        import matplotlib.pyplot as plt
+
+        plt.figure()
+    for f_idx in range(num_freqs):
+        a = np.real(recv_data_list[f_idx][0])
+        b = np.real(recv_data_list[f_idx][1])
+        toa0 = np.argwhere(np.abs(a) >= threshold * np.max(abs(a)))[0][0]
+        toa1 = np.argwhere(np.abs(b) >= threshold * np.max(abs(b)))[0][0]
+        if abs(toa1 - toa0) > head:
+            print(f"problem :(   fidx = {f_idx}")
+            toa = min([toa0, toa1])
+        else:
+            toa = int(0.5 * (toa0 + toa1))
+        start = min([max([0, toa - head]), num_samples - new_num_samples])
+        stop = start + new_num_samples
+        data_trimmed[f_idx][0] = recv_data_list[f_idx][0][start:stop]
+        data_trimmed[f_idx][1] = recv_data_list[f_idx][1][start:stop]
+        if plot:
+            plt.cla()
+            plt.plot(np.real(data_trimmed[f_idx][0]))
+            plt.plot(np.real(data_trimmed[f_idx][1]))
+    return data_trimmed
+
+
+class DataLoader:
+    def __init__(self, verbose: bool = True, plot_flag: bool = False) -> None:
+        self.verbose: bool = verbose
+        self.plot_flag: bool = plot_flag
+        self.recv_data_list: List = []
+        self.p: dict = {
+            "num_freqs": 0,
+            "num_channels": 0,
+            "num_samples": 0,
+            "sampling_rate": 0,
+            "center_freqs": [],
+            "chirp_duration": 0.0,
+            "chirp_bw": 0,
+        }
+
+    def load_mat(self, filename: str) -> None:
+        """
+        Read data and acquisition parameters from .mat file
+        """
+        from scipy.io import loadmat
+
+        data = loadmat(filename)
+        if self.verbose:
+            print("Reading ", data["__header__"])
+        self.recv_data_list = data["data"]
+        num_freqs, num_channels, num_samples = self.recv_data_list.shape
+        self.p.update(
+            {
+                "num_freqs": num_freqs,
+                "num_channels": num_channels,
+                "num_samples": num_samples,
+                "sampling_rate": int(data["sampling_rate"]),
+                "center_freqs": data["frequencies"].flatten(),
+                "chirp_duration": float(data["chirp_duration"]),
+                # / int(data["sampling_rate"]) ** 2,  # (fix bug in acquisition code)
+                "chirp_bw": int(data["chirp_bw"]),
+            }
+        )
+
+        if self.plot_flag:
+            self.plot_data()
+
+    def plot_data(self):
+        import matplotlib.pyplot as plt
+        import numpy as np
+
+        n = self.p["num_samples"]
+        fs = self.p["sampling_rate"]
+        n_freq = self.p["num_freqs"]
+        time_vector = 1 / fs * np.linspace(0, n, num=n)
+
+        legend1_text = []
+        legend2_text = []
+        plt.figure()
+        for ii in range(n_freq):
+            if ii < n_freq / 2:
+                plt.subplot(211)
+                legend1_text.append(f"f {ii+1}")
+            else:
+                plt.subplot(212)
+                legend2_text.append(f"f {ii+1}")
+            plt.plot(
+                time_vector * 1e6,
+                np.real(self.recv_data_list[ii][0]),
+            )
+
+        plt.title("Raw Time-Domain Data")
+        # plt.xlabel("Time [Samples]")
+        plt.xlabel("Time [us]")
+        plt.legend(legend2_text)
+        plt.grid("True")
+        plt.subplot(211)
+        plt.legend(legend1_text)
+        plt.grid("True")
