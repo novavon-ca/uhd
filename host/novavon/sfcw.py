63943b76083ebe5656208e17f1dd0d16777bd7db
Hannah Fogel <hfogel@hotmail.ca>
2024-03-04 15:48:45 -0600

    Updated freq hopping routine

--- host/novavon/sfcw.py
+++ host/novavon/sfcw.py
@@ -7,13 +7,13 @@ import threading
 from typing import Any, List
 import matplotlib.pyplot as plt
 from scipy.io import savemat
+import json 
 
 from utilities import LogFormatter
 # from waveforms import dc_chirp
 # from usrp_settings import usrp_setup, setup_streamers
 
 def setup_device(samp_rate, logger, verbose=False):
-    device_args = "type=b200,recv_frame_size=1024"
     subdev = "A:A A:B"
     clock_source = "internal"
     rx_bw = samp_rate
@@ -25,7 +25,6 @@ def setup_device(samp_rate, logger, verbose=False):
 
     # create the USRP device
     usrp = uhd.usrp.MultiUSRP()
-    #  usrp = uhd.usrp.MultiUSRP(device_args)
 
     # set clocks
     usrp.set_clock_source(clock_source)
@@ -80,9 +79,8 @@ def tx_worker(usrp, tx_streamer, tx_data, tx_md):
         num_tx_samps += tx_streamer.send(tx_data, tx_md) * num_channels
         num_acc_samps += min(total_samps - num_acc_samps, tx_streamer.get_max_num_samps())
 
-def tune_center_freq(usrp, target_freq):
-    global STATE
-    tx_gain = 60
+def tune_center_freq(usrp, target_freq, add_rx_gain):
+    tx_gain = 45
     rx_gain = 45
     tx_chan_idx = 1
 
@@ -92,39 +90,34 @@ def tune_center_freq(usrp, target_freq):
 
     # set freq-dependent gain here
     usrp.set_tx_gain(tx_gain, tx_chan_idx)
-    usrp.set_rx_gain(rx_gain, 0)
-    usrp.set_rx_gain(rx_gain, 1)
+    usrp.set_rx_gain(rx_gain+add_rx_gain[0], 0)
+    usrp.set_rx_gain(rx_gain+add_rx_gain[1]+10 , 1)
     
     # wait until LOs are locked
     while not (usrp.get_rx_sensor("lo_locked", 0).to_bool() and usrp.get_tx_sensor("lo_locked").to_bool()):
         pass
 
-    STATE = "RX_AND_SAVE"
     return
 
 def rx_and_save(usrp, rx_streamer, rx_buffer, rx_md, num_samps, current_freq):
-    global STATE
-
     if current_freq > END_FREQ:
-        STATE = "DONE"
         return
 
-    logger.info("Current freq: %d MHz", current_freq/1e6)
+    # logger.info("Current freq: %d MHz", current_freq/1e6)
     stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.num_done)
     stream_cmd.num_samps = num_samps
     stream_cmd.stream_now = False
-    stream_cmd.time_spec = usrp.get_time_now() + uhd.types.TimeSpec(0.01) #uhd.types.TimeSpec(usrp.get_time_now().get_real_secs() + TX_DELAY)
+    stream_cmd.time_spec = usrp.get_time_now() + uhd.types.TimeSpec(0.01)
     rx_streamer.issue_stream_cmd(stream_cmd)
 
     rx = rx_streamer.recv(rx_buffer, rx_md)
     # logger.info(rx_md)
-    STATE = "TUNE_FREQ"
 
-def generate_tx_data(samp_rate):
+def generate_tx_data(samp_rate, chirp_bw):
     plot_data = False
     chan_list = (0,1)
-    chirp_len_samples = 85000/2
-    bw = 16e6
+    chirp_len_samples = 40000 # 85000 TODO: determine optimal pulse length - for some reason must be >=30000
+    bw = chirp_bw
     n = np.arange(0, chirp_len_samples-1) - chirp_len_samples/2
     t = n/samp_rate
     ampl = 0.8
@@ -138,7 +131,7 @@ def generate_tx_data(samp_rate):
         plt.xlabel('Time [s]')
         plt.title('Chirp source')
 
-    pad_len = 4096
+    pad_len = 4096 # don't change! making this smaller makes the rx'ed signal asymmetric
     chirp = np.pad(chirp, pad_len, 'constant', constant_values=(0))
     
     if plot_data:
@@ -157,22 +150,40 @@ def generate_tx_data(samp_rate):
     tx_md.end_of_burst = False
     tx_md.has_time_spec = True
 
-    chirp_duration = chirp.size * samp_rate
+    chirp_duration = chirp.size / samp_rate
     return tx_data, tx_md, chirp.size, chirp_duration
 
 
+def get_gain_adjustments(filename):
+    with open(filename, "r") as f:
+        gain_table = json.load(f)
+    
+    gain_table = gain_table.get(str(int(CHIRP_BW)), None)
+    if gain_table is not None:
+        gain_ch0 = gain_table["factors"][0]
+        gain_ch1 = gain_table["factors"][1]
+        try:
+            idx = np.argwhere(np.array(gain_table["freqs"])==START_FREQ)[0][0]
+            gain_ch0 = gain_ch0[idx:]
+            gain_ch1 = gain_ch1[idx:]
+            print('Found gain compensation factors')
+        except IndexError:
+            gain_ch0 = np.zeros_like(gain_ch0)
+            gain_ch1 = np.zeros_like(gain_ch1)
+    else:
+        gain_ch0 = [0]*300
+        gain_ch1 = [0]*300
+    return gain_ch0, gain_ch1
+
 def main():
-    global STATE
-    STATE = "TUNE_FREQ"
-    output_filename = 'sfcw_jan12_halfchirplen_switchTxDBoard_B'
-    plot_data = True
-    samp_rate = 16e6
-    tx_gain = 60
-    rx_gain = 45
+    output_filename = 'TEST.mat'
+    plot_data = False
 
-    usrp, tx_streamer, rx_streamer = setup_device(samp_rate, tx_gain, rx_gain, logger, verbose=False)
+    usrp, tx_streamer, rx_streamer = setup_device(SAMP_RATE, logger, verbose=False)
 
-    tx_data, tx_md, chirp_samples, chirp_duration = generate_tx_data(samp_rate)
+    tx_data, tx_md, chirp_samples, chirp_duration = generate_tx_data(SAMP_RATE, CHIRP_BW)
+    
+    gain_ch0, gain_ch1 = get_gain_adjustments("./gain_tables.json")
     
     # create receive buffers
     num_rx_samps = chirp_samples * 3
@@ -180,14 +191,15 @@ def main():
     rx_md = uhd.types.RXMetadata()
     rx_data_list = []
 
-    gain_idx = 0
     freq_list = np.arange(START_FREQ, END_FREQ, FREQ_STEP)
+    assert(len(gain_ch1)>=len(freq_list))
+    usrp.set_time_now(uhd.types.TimeSpec(0.0))
+    start = time.time()
 
     for f_idx, current_freq in enumerate(freq_list):
         rx_buffer = np.zeros((2, num_rx_samps), dtype=np.complex64)
         
-        usrp.set_time_now(uhd.types.TimeSpec(0.0))
-        tune_center_freq(usrp, current_freq)
+        tune_center_freq(usrp, current_freq, [gain_ch0[f_idx], gain_ch1[f_idx]])
         
         tx_thread = threading.Thread(target=tx_worker, args=(usrp, tx_streamer, tx_data, tx_md))
         rx_thread = threading.Thread(target=rx_and_save, args=(usrp, rx_streamer, rx_buffer, rx_md, num_rx_samps, current_freq))
@@ -203,50 +215,65 @@ def main():
 
         rx_data_list.append(rx_buffer)        
     
-    # end = time.time()
+    end = time.time()
+    logger.info(f"Acquisition time: {end-start} s")
 
     if output_filename:
         savemat(output_filename, {
             "data": rx_data_list, 
-            'sampling_rate': samp_rate, 
+            'sampling_rate': SAMP_RATE, 
             'frequencies': freq_list, 
             'chirp_duration': chirp_duration, 
-            'chirp_bw':samp_rate, 
-            'tx_gain': tx_gain, 
-            'rx_gain': rx_gain
+            'chirp_bw':CHIRP_BW
             })
         logger.info(f"Data written to {output_filename}")
     
     if plot_data:
         rx_data_list = np.array(rx_data_list)
-        # print(rx_data_list.shape)
         
         plt.figure()
-        plt.subplot(311)
+        # time domain plots
+        plt.subplot(321)
         plt.title('F1')
-        plt.plot(rx_data_list[0,0,:])
-        plt.plot(rx_data_list[0,1,:])
+        plt.plot(np.real(rx_data_list[30,0,:]))
+        plt.plot(np.real(rx_data_list[30,1,:]))
 
-        plt.subplot(312)
+        plt.subplot(323)
         plt.title('F2')
-        plt.plot(rx_data_list[1,0,:])
-        plt.plot(rx_data_list[1,1,:])
+        plt.plot(np.real(rx_data_list[50,0,:]))
+        plt.plot(np.real(rx_data_list[50,1,:]))
 
-        plt.subplot(313)
+        plt.subplot(325)
         plt.title('F3')
-        plt.plot(rx_data_list[20,0,:])
-        plt.plot(rx_data_list[20,1,:])
+        plt.plot(np.real(rx_data_list[70,0,:]))
+        plt.plot(np.real(rx_data_list[70,1,:]))
+
+        # freq domain plots
+        plt.subplot(322)
+        fft_freqs = np.fft.fftfreq(num_rx_samps, 1/SAMP_RATE)
+        plt.title('F1')
+        plt.plot(fft_freqs, 20*np.log10(np.abs(np.fft.fft(rx_data_list[30,0,:]))))
+        plt.plot(fft_freqs, 20*np.log10(np.abs(np.fft.fft(rx_data_list[30,1,:]))))
+
+        plt.subplot(324)
+        plt.title('F2')
+        plt.plot(fft_freqs, 20*np.log10(np.abs(np.fft.fft(rx_data_list[50,0,:]))))
+        plt.plot(fft_freqs, 20*np.log10(np.abs(np.fft.fft(rx_data_list[50,1,:]))))
+
+        plt.subplot(326)
+        plt.title('F3')
+        plt.plot(fft_freqs, 20*np.log10(np.abs(np.fft.fft(rx_data_list[70,0,:]))))
+        plt.plot(fft_freqs, 20*np.log10(np.abs(np.fft.fft(rx_data_list[70,1,:]))))
 
         plt.show()
 
 
-
-
-
 if __name__ == "__main__":
-    START_FREQ = 800e6
-    END_FREQ = 2e9
-    FREQ_STEP = 16e6
+    START_FREQ = 605e6
+    END_FREQ = 2002e6
+    FREQ_STEP = 15e6
+    CHIRP_BW = 15e6
+    SAMP_RATE = 16e6
     TX_DELAY = 0.012
 
     global logger
